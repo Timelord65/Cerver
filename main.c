748707c85ba87ee58e073405fe5a5febfe2188ca
Timelorde <praneethchokka@gmail.com>
2024-09-08 01:01:57 +0530

    Added multi-threading for parallel client sessions

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <fcntl.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
-/* #include <stdlib.h> */
+#include <stdlib.h>
 #include <string.h>
 #include <sys/sendfile.h>
 #include <sys/socket.h>
@@ -12,6 +14,8 @@
 
 #define INFO "Information Logging \n"
 #define ERR "Error Logging \n"
+
+int socket_fd;
 
 void print_message(char *type, char *message) {
   time_t t = time(NULL);
@@ -24,14 +28,66 @@ void print_message(char *type, char *message) {
   fflush(stdout);
 }
 
+void handle_sigint(int signal) {
+  print_message(INFO, "Shutting down server");
+  close(socket_fd);
+  exit(0);
+}
+
+void *handle_connection(void *client_fd_ptr) {
+  char buffer[256] = {0};
+  int client_fd = *(int *)client_fd_ptr;
+  free(client_fd_ptr);
+  /*
+   * return data from recv of the format:
+   * GET /file.html
+   */
+  recv(client_fd, buffer, 256, 0);
+
+  int open_fd = open("./static/index.html", O_RDONLY);
+
+  struct stat file_stat;
+  if (fstat(open_fd, &file_stat) < 0) {
+    perror("fstat failed");
+    print_message(ERR, "fstat error has occured");
+    return NULL;
+  }
+
+  off_t file_size = file_stat.st_size;
+
+  // Convert file size to a string
+  char content_length_header[64];
+  snprintf(content_length_header, sizeof(content_length_header),
+           "Content-Length: %ld\r\n", file_size);
+
+  const char *http_response = "HTTP/1.1 200 OK\r\n"
+                              "Content-Type: text/html\r\n";
+
+  send(client_fd, http_response, strlen(http_response), 0);
+  send(client_fd, content_length_header, strlen(content_length_header), 0);
+  send(client_fd, "\r\n", 2, 0); // End of headers
+
+  sendfile(client_fd, open_fd, NULL, file_size);
+
+  close(client_fd);
+  close(open_fd);
+
+  return NULL;
+}
+
 int main() {
+
+  signal(SIGINT, handle_sigint);
 
   const char *STATIC_PATH = "./static/";
   print_message(INFO, "Starting Server Execution");
 
-  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-  struct sockaddr_in addr = {AF_INET, 0x901f, 0};
+  struct sockaddr_in addr = {0};
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(8080);
 
   int bind_ret = bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
 
@@ -45,6 +101,8 @@ int main() {
 
   if (listen_ret == -1) {
     print_message(ERR, "Unsuccessful in assigning passive socket and ip");
+    close(socket_fd);
+    return 1;
   }
   print_message(
       INFO,
@@ -52,41 +110,34 @@ int main() {
 
   while (1) {
 
-    int client_fd = accept(socket_fd, 0, 0);
+    int *client_fd = (int *)malloc(sizeof(int));
+    *client_fd = accept(socket_fd, 0, 0);
 
-    char buffer[256] = {0};
-    /*
-     * return data from recv of the format:
-     * GET /file.html
-     */
-    recv(client_fd, buffer, 256, 0);
-
-    int open_fd = open("./static/index.html", O_RDONLY);
-
-    struct stat file_stat;
-    if (fstat(open_fd, &file_stat) < 0) {
-      perror("fstat failed");
-      return 1;
+    if (*client_fd == -1) {
+      free(client_fd);
+      print_message(ERR, "Client File Descriptor Failed");
+      continue;
     }
 
-    off_t file_size = file_stat.st_size;
+    pthread_t thread_id;
+    int thread_create_ret =
+        pthread_create(&thread_id, NULL, handle_connection, client_fd);
 
-    // Convert file size to a string
-    char content_length_header[64];
-    snprintf(content_length_header, sizeof(content_length_header),
-             "Content-Length: %ld\r\n", file_size);
-
-    const char *http_response = "HTTP/1.1 200 OK\r\n"
-                                "Content-Type: text/html\r\n";
-
-    send(client_fd, http_response, strlen(http_response), 0);
-    send(client_fd, content_length_header, strlen(content_length_header), 0);
-    send(client_fd, "\r\n", 2, 0); // End of headers
-
-    sendfile(client_fd, open_fd, NULL, file_size);
-
-    close(client_fd);
-    close(open_fd);
+    if (thread_create_ret != 0) {
+      print_message(ERR, "Thread cration failed");
+      close(*client_fd);
+      free(client_fd);
+      continue;
+    } else {
+      print_message(INFO, "Thread creation successful. Another client instance "
+                          "is being processed.");
+    }
+    int thread_detach_ret = pthread_detach(thread_id);
+    if (thread_detach_ret != 0) {
+      print_message(
+          ERR,
+          "Thread detachment successful. Client session closed successfully");
+    }
   }
 
   close(socket_fd);
